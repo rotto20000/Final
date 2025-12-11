bbe6fad5b19334be3b1d514fd68dfcfaff870442
rotto20000 <s-ryan.otto@LWTech.edu>
2025-12-11 11:22:33 -0800

    Update LoginSeleniumTests.cs

--- LoginSeleniumTests.cs
+++ LoginSeleniumTests.cs
@@ -23,6 +23,8 @@ public class LoginSeleniumTest
     public void SuccessfulTest()
     {
         driver.Navigate().GoToUrl("https://www.letsusedata.com/login");
+
+        var loginUrl = driver.Url;
         driver.FindElement(By.Id("txtUser")).Click();
         driver.FindElement(By.Id("txtUser")).Clear();
         driver.FindElement(By.Id("txtUser")).SendKeys("test1");
@@ -32,12 +34,19 @@ public class LoginSeleniumTest
         driver.FindElement(By.Id("txtPassword")).SendKeys("Test12456");
         
         driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+        Thread.Sleep(2000);
+
+        Assert.AreNotEqual(loginUrl, driver.Url);
+
+
     }
     
     [TestMethod]
     public void UnsuccessfulTest()
     {
         driver.Navigate().GoToUrl("https://www.letsusedata.com/login");
+
+        var loginUrl = driver.Url;
         driver.FindElement(By.Id("txtUser")).Click();
         driver.FindElement(By.Id("txtUser")).Clear();
         driver.FindElement(By.Id("txtUser")).SendKeys("test1");
@@ -47,5 +56,9 @@ public class LoginSeleniumTest
         driver.FindElement(By.Id("txtPassword")).SendKeys("test1234");
         
         driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        Thread.Sleep(2000);
+
+        Assert.AreEqual(loginUrl, driver.Url);
     }
 }
