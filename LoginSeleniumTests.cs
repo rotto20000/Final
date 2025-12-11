3f96c0752aa81c6dc5215f8389772ad37f578733
rotto20000 <s-ryan.otto@LWTech.edu>
2025-12-10 23:41:56 -0800

    LoginSeleniumTests.cs

---
+++ LoginSeleniumTests.cs
@@ -0,0 +1,51 @@
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+[TestClass]
+public class LoginSeleniumTest
+{
+    private IWebDriver driver;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        driver = new ChromeDriver();
+    }
+
+    [TestCleanup]
+    public void TearDown()
+    {
+        driver.Quit();
+    }
+    
+    [TestMethod]
+    public void SuccessfulTest()
+    {
+        driver.Navigate().GoToUrl("https://www.letsusedata.com/login");
+        driver.FindElement(By.Id("txtUser")).Click();
+        driver.FindElement(By.Id("txtUser")).Clear();
+        driver.FindElement(By.Id("txtUser")).SendKeys("test1");
+        
+        driver.FindElement(By.Id("txtPassword")).Click();
+        driver.FindElement(By.Id("txtPassword")).Clear();
+        driver.FindElement(By.Id("txtPassword")).SendKeys("Test12456");
+        
+        driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+    }
+    
+    [TestMethod]
+    public void UnsuccessfulTest()
+    {
+        driver.Navigate().GoToUrl("https://www.letsusedata.com/login");
+        driver.FindElement(By.Id("txtUser")).Click();
+        driver.FindElement(By.Id("txtUser")).Clear();
+        driver.FindElement(By.Id("txtUser")).SendKeys("test1");
+        
+        driver.FindElement(By.Id("txtPassword")).Click();
+        driver.FindElement(By.Id("txtPassword")).Clear();
+        driver.FindElement(By.Id("txtPassword")).SendKeys("test1234");
+        
+        driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+    }
+}
