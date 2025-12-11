4e368b5eef7def165583e2e958dc7a672b1c1446
rotto20000 <s-ryan.otto@LWTech.edu>
2025-12-11 11:24:26 -0800

    Update CanDriveTests.cs

--- CanDriveTests.cs
+++ CanDriveTests.cs
@@ -2,33 +2,58 @@
 [TestClass]
 public class CanDriveTests
 {
+
     [TestMethod]
     public void Method1()
     {
-        Assert.IsFalse(CanDriveHelper.canDrive(0));
+        Assert.IsFalse(CanDriveHelper.canDrive(-1));
     }
 
     [TestMethod]
     public void Method2()
     {
-        Assert.IsFalse(CanDriveHelper.canDrive(15));
+        Assert.IsFalse(CanDriveHelper.canDrive(0));
     }
 
     [TestMethod]
     public void Method3()
     {
-        Assert.IsTrue(CanDriveHelper.canDrive(16));
+        Assert.IsFalse(CanDriveHelper.canDrive(1));
     }
 
     [TestMethod]
     public void Method4()
     {
-        Assert.IsTrue(CanDriveHelper.canDrive(17));
+        Assert.IsFalse(CanDriveHelper.canDrive(15));
     }
 
     [TestMethod]
     public void Method5()
     {
+        Assert.IsTrue(CanDriveHelper.canDrive(16));
+    }
+
+    [TestMethod]
+    public void Method6()
+    {
+        Assert.IsTrue(CanDriveHelper.canDrive(17));
+    }
+
+    [TestMethod]
+    public void Method7()
+    {
+        Assert.IsTrue(CanDriveHelper.canDrive(99));
+    }
+
+    [TestMethod]
+    public void Method8()
+    {
         Assert.IsTrue(CanDriveHelper.canDrive(100));
+    }
+
+    [TestMethod]
+    public void Method9()
+    {
+        Assert.IsTrue(CanDriveHelper.canDrive(101));
     }
 }
